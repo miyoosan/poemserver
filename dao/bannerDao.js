36640272e81ac5ffa2c337ab64cba86e4a442d92
bb s <424626154@qq.com>
2017-12-20 12:40:21 +0800

    banner排序

--- dao/bannerDao.js
+++ dao/bannerDao.js
@@ -9,7 +9,7 @@ module.exports = {
 	 * 查询banners
 	 */
 	queryNewestBanner(fromid,callback){
-		var sql = 'SELECT * FROM '+BANNER_TABLE+' WHERE id>? AND state = 1 AND del = 0 ORDER BY id DESC ';
+		var sql = 'SELECT * FROM '+BANNER_TABLE+' WHERE id>? AND state = 1 AND del = 0 ORDER BY id ASC ';
         pool.getConnection(function(err, connection) {
             connection.query(sql, fromid, function(err, result) {
             	callback(err, result)
